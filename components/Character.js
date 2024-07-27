d75c4f3d9db2a9be87a0eb608cd0a49b3bf22454
derek2403 <derekliew0@gmail.com>
2024-07-27 21:23:53 +0800

    added animation

---
+++ components/Character.js
@@ -0,0 +1,68 @@
+// components/Character.js
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useKeyPress from '../hooks/useKeyPress';
+import Image from 'next/image';
+
+
+const Character = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [sprite, setSprite] = useState('/csy1.png'); // Default sprite
+  const router = useRouter(); // Use the useRouter hook
+
+  const moveLeft = useKeyPress('a');
+  const moveRight = useKeyPress('d');
+  const jump = useKeyPress('w');
+  const navigate = useKeyPress(' '); // Detect spacebar press
+
+  useEffect(() => {
+    let interval;
+    if (moveLeft) {
+      setSprite('/csy2.png'); // Sprite for moving left
+      interval = setInterval(() => {
+        setPosition((pos) => ({ ...pos, x: pos.x - 5 }));
+      }, 50);
+    }
+
+    if (moveRight) {
+      setSprite('/csy1.png'); // Sprite for moving right
+      interval = setInterval(() => {
+        setPosition((pos) => ({ ...pos, x: pos.x + 5 }));
+      }, 50);
+    }
+
+    if (jump) {
+      setPosition((pos) => ({ ...pos, y: pos.y - 10 }));
+      setTimeout(() => {
+        setPosition((pos) => ({ ...pos, y: pos.y + 10 }));
+      }, 300); // Simulate jump duration
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [moveLeft, moveRight, jump]);
+
+  useEffect(() => {
+    if (navigate) {
+      router.push('/room'); // Replace '/newpage' with your desired route
+    }
+  }, [navigate, router]);
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: `calc(50% + ${position.x}px)`, // Center horizontally within the viewport
+        bottom: '27.5%', // Position relative to the bottom
+        transform: 'scale(2)', // Adjust the scale as needed
+        transformOrigin: 'center center', // Ensure the character is scaled from its center
+        transition: 'left 0.1s, bottom 0.3s',
+      }}
+    >
+      <Image src={sprite} alt="Character" width={50} height={50} />
+    </div>
+  );
+};
+
+export default Character;
---
+++ pages/room.js
@@ -0,0 +1,10 @@
+// pages/room.js
+import Room from '../components/Room';
+
+export default function RoomPage() {
+  return (
+    <div>
+      <Room />
+    </div>
+  );
+}
